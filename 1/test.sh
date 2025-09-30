82258a33540687d1cc83025782c4bf1fac271dc6
heiheiha798 <2300012738@stu.pku.edu.cn>
2025-09-30 13:56:32 +0800

    .

--- 1/test.sh
+++ 1/test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-./build/bin/dpll_solver unif-c1200-v600-s351323111.cnf
-
 ./build/bin/dpll_solver unif-c500-v250-s453695930.cnf
+./build/bin/dpll_solver unif-c1200-v600-s351323111.cnf
